257ce24ccb46dc643a8b9bebaffbc3d00e66ed57
Kumar <kumarkmr>
2023-09-14 15:29:39 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LogInPage from './Components/LogInPage';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>LogIn Page</h1>
+
+     <LogInPage />
+    </div>
+  );
+}
+
+export default App;
